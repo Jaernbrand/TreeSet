268e456305212830ee0c4c8669f3e51ba0ecd4f4
Gouvernator <tomassandberg86@hotmail.com>
2015-03-09 17:38:19 +0100

    vilket bidrag FANTASTISKT!

--- src/MyTreeSet.java
+++ src/MyTreeSet.java
@@ -1,6 +1,7 @@
 
 import java.util.Iterator;
 import java.util.Comparator;
+import java.util.Stack;
 
 
 /**
@@ -27,9 +28,51 @@ public class MyTreeSet<T> {
 		
 	}
 	
-	public void contains(T element){
-		
-	}
+	
+	
+	public boolean contains(T element){
+		if(element == null){
+			throw new NullPointerException("Element can't be null.");
+		}
+		if(root == null){
+			throw new NullPointerException("Tree is empty.");
+		}
+		if(comparator == null)
+			return containsComparable(root, element);
+		else
+			return containsComparator(root, element);
+	}//contains
+	
+	private boolean containsComparable(Node<T> toSearch, T element){
+		if(toSearch == null){
+			return false;
+		}
+		if(toSearch.getValue().compareTo(element) == 0){
+			return true;
+		}
+		if(toSearch.getValue().compareTo(element) < 0){
+			return containsComparable(toSearch.getRightChild(), element);
+		}else{ // > 0
+			return containsComparable(toSearch.getLeftChild(), element);
+		}
+	}//containsComparable
+	
+	
+	
+	private boolean containsComparator(Node<T> toSearch, T element){
+		if(toSearch == null){
+			return false;
+		}
+		if(comparator.compare(toSearch.getValue(), element) == 0){
+			return true;
+		}
+		if(comparator.compare(toSearch.getValue(), element) < 0){
+			return containsComparator(toSearch.getRightChild(), element);
+		}else{ // > 0
+			return containsComparator(toSearch.getLeftChild(), element);
+		}
+	}//containsComparator
+	
 	
 	public void remove(T element){
 		

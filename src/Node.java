bc2019c1a1345b298ac52b3c3ba9aad4ab83695b
Gouvernator <tomassandberg86@hotmail.com>
2015-03-09 16:02:21 +0100

    Node

---
+++ src/Node.java
@@ -0,0 +1,63 @@
+
+public class Node<T> {
+
+	T value;
+	private Node<T> leftChild;
+	private Node<T> rightChild;
+	
+	private Node<T> nextSmallest;
+	private Node<T> nextLargest;
+	
+	
+	Node(T value, Node<T> nextSmallest, Node<T> nextLargest){
+		this.value = value;
+		this.nextSmallest = nextSmallest;
+		this.nextLargest = nextLargest;
+		
+		leftChild = null;
+		rightChild = null;
+	}
+	
+	
+	T getValue(){
+		return value;
+	}
+	
+	Node<T> getLeftChild(){
+		return leftChild;
+	}
+	
+	void setLeftChild(Node<T> leftChild){
+		this.leftChild = leftChild;
+	}
+	
+	Node<T> getRightChild(){
+		return rightChild;
+	}
+	
+	void setRightChild(Node<T> rightChild){
+		this.rightChild = rightChild;
+	}
+	
+	Node<T> getNextSmallest(){
+		return nextSmallest;
+	}
+	
+	void setNextSmallest(Node<T> nextSmallest){
+		this.nextSmallest = nextSmallest;
+	}
+	
+	Node<T> getNextLargest(){
+		return nextLargest;
+	}
+	
+	void setNextLargest(Node<T> nextLargest){
+		this.nextLargest = nextLargest;
+	}
+	
+	
+}
+
+
+
+

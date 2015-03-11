a738b54fb94c99ec3cee7055e51290c7dc3da71f
Jaernbrand <henrikjarnbrand@gmail.com>
2015-03-11 10:52:06 +0100

    Skrivit mer dokumentation.

--- src/treeset/Node.java
+++ src/treeset/Node.java
@@ -2,7 +2,11 @@
 package treeset;
 
 /**
- * The nodes represent the nodes in the class MyTreeSet.
+ * The nodes represent the nodes in the class MyTreeSet. It includes one value
+ * of the generic type T and has two children. The class is also used as
+ * node in the double-linked list spanning the tree. The attributes nextSmallest
+ * and nextLargest are references to the previous node and the next node in the
+ * linked list.
  * 
  * @param <T>
  * Generic type argument, represents the value to store in each node. 
@@ -24,15 +28,6 @@ class Node<T> {
 	
 	Node(T value){
 		this.value = value;
-	}
-	
-	Node(T value, Node<T> nextSmallest, Node<T> nextLargest){
-		this.value = value;
-		this.nextSmallest = nextSmallest;
-		this.nextLargest = nextLargest;
-		
-		leftChild = null;
-		rightChild = null;
 	}
 	
 	void setValue(T value){
@@ -59,18 +54,38 @@ class Node<T> {
 		this.rightChild = rightChild;
 	}
 	
+	/**
+	 * Gets the previous node in the linked list, which also contains the next
+	 * smallest value.
+	 * @return
+	 * - node containing the next smallest value
+	 */
 	Node<T> getNextSmallest(){
 		return nextSmallest;
 	}
 	
+	/**
+	 * Sets the reference to the previous node in the linked list. The previous 
+	 * node should contain the next smallest value. 
+	 */
 	void setNextSmallest(Node<T> nextSmallest){
 		this.nextSmallest = nextSmallest;
 	}
 	
+	/**
+	 * Gets the next node in the linked list, which also contains the next
+	 * largest value.
+	 * @return
+	 * - node containing the next largest value
+	 */
 	Node<T> getNextLargest(){
 		return nextLargest;
 	}
 	
+	/**
+	 * Sets the reference to the next node in the linked list. The next 
+	 * node should contain the next largest value. 
+	 */
 	void setNextLargest(Node<T> nextLargest){
 		this.nextLargest = nextLargest;
 	}
